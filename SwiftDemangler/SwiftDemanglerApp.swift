010113ad3ef22ba85cbb8962878a50180195490e
Bobby Galli <bobbyg603@gmail.com>
2024-02-01 10:59:51 -0500

    Initial Commit

---
+++ SwiftDemangler/SwiftDemanglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemanglerApp.swift
+//  SwiftDemangler
+//
+//  Created by Robert Galli on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemanglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
